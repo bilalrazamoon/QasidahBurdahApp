dbf6da7340c78c11930f2f93a9522ea2f0721301
hafizbilal112 <hafizbilal112@gmail.com>
2015-04-18 15:25:26 +0500

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -5,46 +5,83 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var sh = require('shelljs');
-
+var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 var paths = {
-  sass: ['./scss/**/*.scss']
+    sass: ['./scss/**/*.scss'],
+    lib: [
+        './bower_components/jquery/dist/jquery.js',
+        './bower_components/jquery.easing/js/jquery.easing.js',
+        './bower_components/jquery.ui-to-top/js/jquery.ui.totop.js',
+        './bower_components/owl.carousel/dist/owl.carousel.js',
+        './bower_components/angular/angular.js'
+    ]
 };
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        port: "8100",
+        server: {
+            baseDir: "./"
+        }
     });
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', reload);
+    gulp.watch("./js/*.js").on('change', reload);
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('copyfonts', function() {
+    gulp.src('./bower_components/components-font-awesome/fonts/**/*.{otf,ttf,woff,eof,svg}')
+        .pipe(gulp.dest('./fonts'));
+});
+
+gulp.task('lib', function() {
+    return gulp.src(paths.lib)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe(uglify())
+        .pipe(rename('vendor.min.js'))
+        .pipe(gulp.dest('js/'));
+});
+
+gulp.task('sass', function (done) {
+    gulp.src('./scss/app.scss')
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(gulp.dest('./css/'))
+        .pipe(reload({stream: true}))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./css/'))
+        .on('end', done);
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('install', ['git-check'], function () {
+    return bower.commands.install()
+        .on('log', function (data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+gulp.task('git-check', function (done) {
+    if (!sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
 });
---
+++ js/app.js
@@ -0,0 +1,27 @@
+angular.module('app', [])
+    .run(function ($rootScope) {
+        $rootScope.appName="Qasidah Burdah";
+        $rootScope.appVersion="1.0";
+        $rootScope.features=[
+            {title:"Beautiful, modern design",desc:"Duis bibendum diam non erat facilaisis tincidunt.",icon:"fa-mobile-phone"},
+            {title:"Beautiful, modern design",desc:"Duis bibendum diam non erat facilaisis tincidunt.",icon:"fa-mobile-phone"},
+            {title:"Beautiful, modern design",desc:"Duis bibendum diam non erat facilaisis tincidunt.",icon:"fa-mobile-phone"},
+            {title:"Beautiful, modern design",desc:"Duis bibendum diam non erat facilaisis tincidunt.",icon:"fa-mobile-phone"},
+        ];
+    })
+    .directive('slider', function () {
+        return {
+            restrict: "A",
+            link: function ($scope, $element, $attrs) {
+                $element.owlCarousel({
+                    items: 1,
+                    lazyLoad: true,
+                    autoPlay: true,
+                    navigation: false,
+                    navigationText: false,
+                    pagination: true
+                });
+            }
+        }
+    })
+;
